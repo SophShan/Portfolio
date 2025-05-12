2755690e7a9d2dca8b9bcbd9f791fd99fe0c4dfb
Sophia Shantharupan <Sophia@MacBook-Air.local>
2025-05-12 12:27:35 -0400

    remove avif compression 2

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -281,7 +281,7 @@ const Featured = () => {
               title
               cover {
                 childImageSharp {
-                  gatsbyImageData(width: 700, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+                  gatsbyImageData(width: 700, placeholder: BLURRED, formats: [AUTO, WEBP])
                 }
               }
               tech
